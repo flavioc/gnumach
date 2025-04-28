780d2561b24aa317bbdf84effda177d775f852af
Samuel Thibault <samuel.thibault@ens-lyon.org>
2025-04-28 15:06:32 +0200

    tests: Also support test kernel not being called gnumach

--- tests/user-qemu.mk
+++ tests/user-qemu.mk
@@ -184,7 +184,7 @@ tests/test-%.iso: tests/module-% $(GNUMACH) $(srcdir)/tests/grub.cfg.single.temp
 		    -e "s/GNUMACHARGS/$(GNUMACH_ARGS)/g"	\
 		    -e "s/TEST_START_MARKER/$(TEST_START_MARKER)/g"	\
 		>$(builddir)/tests/isofiles-$*/boot/grub/grub.cfg
-	cp $(GNUMACH) $< $(builddir)/tests/isofiles-$*/boot/
+	cp $(GNUMACH) $< $(builddir)/tests/isofiles-$*/boot/gnumach
 	grub-mkrescue -o $@ $(builddir)/tests/isofiles-$*
 	rm -rf $(builddir)/tests/isofiles-$*
 
