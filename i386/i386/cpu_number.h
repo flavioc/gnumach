597c891330810eb4c152c138d57615557c073868
Damien Zammit <damien@zamaudio.com>
2026-02-14 04:01:20 +0000

    locore: Remove all instances of CPU_NUMBER_NO_*
    This relies on gs segment percpu pointer always being
    available in critical contexts in smp builds.
    
    TESTED:
     - i386+SMP boots to shell with -smp 6
     - Passes all i386 CI.
    
    Message-ID: <20260214040102.2503708-1-damien@zamaudio.com>

--- i386/i386/cpu_number.h
+++ i386/i386/cpu_number.h
@@ -41,69 +41,6 @@
 #define	CX(addr, reg)	addr(,reg,8)
 #endif
 
-/* Slow version, always works. */
-#ifdef __i386__
-#define	CPU_NUMBER_NO_STACK(reg)	\
-	movl	%cs:lapic, reg		;\
-	movl	%cs:APIC_ID(reg), reg	;\
-	shrl	$24, reg		;\
-	andl	%cs:apic_id_mask, reg	;\
-	movl	%cs:CX(cpu_id_lut, reg), reg
-#endif
-#ifdef __x86_64__
-/* Specify 8/32/64 bit variants of clob and 32/64 variants of reg */
-#define	CPU_NUMBER_NO_STACK(clob8, clob32, clob64, reg32, reg64)\
-	movq	%cs:lapic, clob64	;\
-	movl	%cs:APIC_ID(clob64), clob32	;\
-	shrl	$24, clob32		;\
-	andb	%cs:apic_id_mask, clob8	;\
-	leaq	cpu_id_lut(%rip), reg64	;\
-	movl	%cs:(reg64, clob64, 8), reg32
-#endif
-
-/* Fast version, requires a stack */
-#ifdef __i386__
-/* Never call CPU_NUMBER_NO_GS(%esi) */
-#define CPU_NUMBER_NO_GS(reg)		\
-	pushl	%esi		;\
-	pushl	%eax		;\
-	pushl	%ebx		;\
-	pushl	%ecx		;\
-	pushl	%edx		;\
-	movl	$1, %eax	;\
-	cpuid			;\
-	shrl	$24, %ebx	;\
-	andl	%cs:apic_id_mask, %ebx	;\
-	movl	%cs:CX(cpu_id_lut, %ebx), %esi	;\
-	popl	%edx		;\
-	popl	%ecx		;\
-	popl	%ebx		;\
-	popl	%eax		;\
-	movl	%esi, reg	;\
-	popl	%esi
-#endif
-#ifdef __x86_64__
-/* Never call CPU_NUMBER_NO_GS(%rsi) */
-#define CPU_NUMBER_NO_GS(reg)		\
-	pushq	%rsi		;\
-	pushq	%rax		;\
-	pushq	%rbx		;\
-	pushq	%rcx		;\
-	pushq	%rdx		;\
-	movl	$1, %eax	;\
-	cpuid			;\
-	shrl	$24, %ebx	;\
-	andb	%cs:apic_id_mask, %bl	;\
-	leaq	cpu_id_lut(%rip), %rsi	;\
-	movl	%cs:(%rsi, %rbx, 4), %esi ;\
-	popq	%rdx		;\
-	popq	%rcx		;\
-	popq	%rbx		;\
-	popq	%rax		;\
-	movq	%rsi, reg	;\
-	popq	%rsi
-#endif
-
 /* Fastest version, requires gs being set up */
 #define CPU_NUMBER(reg)	\
 	movl    MY(CPU_ID), reg;
@@ -130,16 +67,6 @@ static inline int cpu_number(void)
 
 #define MY(stm)		(percpu_array + PERCPU_##stm)
 
-#ifdef __x86_64__
-#define	CPU_NUMBER_NO_STACK(clob8, clob32, clob64, reg32, reg64) \
-	xorl	reg32, reg32
-#endif
-#ifdef __i386__
-#define CPU_NUMBER_NO_STACK(reg) \
-	xorl	reg, reg
-#endif
-#define	CPU_NUMBER_NO_GS(reg) \
-	xor	reg, reg
 #define	CPU_NUMBER(reg) \
 	xor	reg, reg
 #define	CX(addr,reg)	addr
